204d371f2a4127035baf55cedb87009870b0ae8c
valencia_fly <pengwanring@live.com>
2023-04-21 06:47:40 +0800

    feat(def): ✨ add macro ARRAY_SIZE

--- include/cmd_args/base/def.h
+++ include/cmd_args/base/def.h
@@ -3,3 +3,10 @@
 #define CMD_ARGS_NAMESPACE_BEGIN namespace cmd_args {
 #define CMD_ARGS_NAMESPACE_END   }  // end of namespace cmd_args
 #define CMD_ARGS_NAMESPACE(_X)   cmd_args::##_X
+
+namespace detail {
+template <typename T, unsigned int N>
+char (&fakeArraySize(T (&)[N]))[N];
+}
+
+#define ARRAY_SIZE(_A) sizeof(::detail::fakeArraySize(_A))

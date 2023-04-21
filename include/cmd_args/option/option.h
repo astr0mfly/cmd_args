abcf8cfb7b5a405f23b9fac645835cd51260b103
valencia_fly <pengwanring@live.com>
2023-04-21 23:53:07 +0800

    style: 📄 updated lisence

--- include/cmd_args/option/option.h
+++ include/cmd_args/option/option.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright[2023][ValenciaFly]
+ * Email: pengwanring@live.com
+ *
+ *     Licensed under the Apache License,
+ *     Version 2.0(the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include <algorithm>
@@ -34,7 +51,8 @@ struct option_value : option
     std::string         type;   // bool, int, double, std::string
     mutable std::string value;  // the defaut value
 
-    option_value(char sname, std::string lname, std::string help, std::string type, std::string value)
+    option_value(
+        char sname, std::string lname, std::string help, std::string type, std::string value)
         : option(sname, std::move(lname), std::move(help))
         , type(std::move(type))
         , value(std::move(value))
@@ -54,7 +72,8 @@ struct option_action : option
 {
     std::function<void(void)> callback;
 
-    option_action(char sname, std::string lname, std::string help, std::function<void(void)> callback)
+    option_action(
+        char sname, std::string lname, std::string help, std::function<void(void)> callback)
         : option(sname, std::move(lname), (std::move(help)))
         , callback(std::move(callback))
     {}
@@ -75,10 +94,10 @@ class value_opt_mgr : opt_mgr_itf
 public:
     option const *doGet(char _Key) const noexcept override
     {
-        auto it =
-            std::find_if(m_vecValues.cbegin(), m_vecValues.cend(), [_Key](decltype(m_vecValues)::const_reference _V) {
-                return _V.short_name == _Key;
-            });
+        auto it = std::find_if(
+            m_vecValues.cbegin(),
+            m_vecValues.cend(),
+            [_Key](decltype(m_vecValues)::const_reference _V) { return _V.short_name == _Key; });
         if (it == m_vecValues.cend()) return nullptr;
 
         return std::addressof(*it);
@@ -86,10 +105,10 @@ public:
 
     option const *doGet(std::string _Key) const noexcept override
     {
-        auto it =
-            std::find_if(m_vecValues.cbegin(), m_vecValues.cend(), [_Key](decltype(m_vecValues)::const_reference _V) {
-                return _V.long_name == _Key;
-            });
+        auto it = std::find_if(
+            m_vecValues.cbegin(),
+            m_vecValues.cend(),
+            [_Key](decltype(m_vecValues)::const_reference _V) { return _V.long_name == _Key; });
         if (it == m_vecValues.cend()) return nullptr;
 
         return std::addressof(*it);
@@ -97,12 +116,18 @@ public:
 
     void push(char sname, std::string lname, std::string help, std::string type, std::string value)
     {
-        m_vecValues.emplace_back(sname, std::move(lname), std::move(help), std::move(type), std::move(value));
+        m_vecValues.emplace_back(
+            sname,
+            std::move(lname),
+            std::move(help),
+            std::move(type),
+            std::move(value));
     }
 
     void push(std::string lname, std::string help, std::string type, std::string value)
     {
-        m_vecValues.emplace_back(std::move(lname), std::move(help), std::move(type), std::move(value));
+        m_vecValues
+            .emplace_back(std::move(lname), std::move(help), std::move(type), std::move(value));
     }
 
     option_value const *find(char _Key) const
